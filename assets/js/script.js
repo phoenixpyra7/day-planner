d9c75656eb53a993610aa76afb227b3353c33a8d
Seven <phoenixpyra7@gmail.com>
2023-11-18 16:09:26 -0600

    playing with codes that dont want to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@
 // var present =
 // var future =
 var savebtn = "save";
+var currentDay = dayjs();
+var currentHour = dayjs().hour();
 
 
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
-  // $(function (: data() Selector) 
+  // $(function (: data() Selector) ?????????????????????????????????????????????????????????????
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -17,12 +19,12 @@ var savebtn = "save";
     //event listener to proceed to the questions
 //     saveBtn.addEventListener('click', trigger () {
 //       if (.trigger.target.matches('button'));
-// // nothing is working
+// // nothing is working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //     });
 
 
   //
-  // timeBlock attr = ("past,present,future")
+  // timeBlock attr = ("past,present,future") ????????????????????????????????????????????
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -32,6 +34,7 @@ var savebtn = "save";
 
   //How can Day.js be used to get the current hour in 24-hour time?
   //
+  // var currentHour = dayjs().hour(); ??????????????????????????????????????????????????
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
