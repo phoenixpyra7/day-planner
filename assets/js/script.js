6fb02931aafdc5edc49cdceb69f12c26957afde4
Seven <phoenixpyra7@gmail.com>
2023-11-19 17:21:45 -0600

    removed all javascript code, replaced with jQuery code as I missunderstood all code was to be jQuery.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
-// var past = 
-// var present =
-// var future =
-var savebtn = "save";
-var currentDay = dayjs();
-var currentHour = dayjs().hour();
-var pastTime = timeBlock.row.past();
-var presentTime = timeBlock.row.present();
-var futureTime = timeBlock.row.future();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function (: data() Selector) ?????????????????????????????????????????????????????????????
+
+
+$(document).ready(function () { //Wrapped code to interact with the DOM 
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY")); //selected date format
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function () { //used time block from html, 
+        var blockHour = parseInt($(this).attr("id").split("-")[1]); // parse Int to round to whole, split hour from the 9
+        console.log(currentHour + blockHour);
+        if (currentHour > blockHour) { // if current time from the block time is less than current time
+            $(this).addClass("past") // then it is considered the past
+        } else if (currentHour === blockHour) { // otherwise if the current time is same as block time
+            $(this).addClass("present") // then its present time
+        } else if (currentHour < blockHour) {
+            $(this).addClass("future") // then its considered the future
+        }
+    })
+    // console.log("DOM",$(this));
+    $(".saveBtn").on("click", function () {
+        // console.log("button",$(this))
+    })
+})
+
+// console.log("global",$(this))
+
+//$(function (: data() Selector) ?????????????????????????????????????????????????????????????
+
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
 // function? How can DOM traversal be used to get the "hour-x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
-//event listener to proceed to the questions
-//     saveBtn.addEventListener('click', trigger () {
-//       if (.trigger.target.matches('button'));
-// // nothing is working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//     });
+// event listener to proceed to the questions
+// saveBtn.addEventListener('click', trigger () {
+//   if (.trigger.target.matches('button'));
+
+// });
 
 
 //
@@ -36,7 +49,7 @@ var futureTime = timeBlock.row.future();
 
 //How can Day.js be used to get the current hour in 24-hour time?
 //
-// var currentHour = dayjs().hour(); ??????????????????????????????????????????????????
+//var currentHour = dayjs().hour(); 
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
