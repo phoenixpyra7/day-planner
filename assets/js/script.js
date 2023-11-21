6f3d0abf62e102562ca84b64bae818208bcf7502
Seven <phoenixpyra7@gmail.com>
2023-11-20 21:44:45 -0600

    fixed local storage code and moved the retrieve to be within main function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () { //Wrapped code to interact with the DOM
     var currentHour = dayjs().hour();
     $(".time-block").each(function () { //used time block from html, 
         var blockHour = parseInt($(this).attr("id").split("-")[1]); // parse Int to round to whole, split hour from the 9 ref html
-        // console.log(currentHour + blockHour);
+        console.log(blockHour);
         if (currentHour > blockHour) { // if current time from the block time is grtr than current time
             $(this).addClass("past") // then it is considered the past
         } else if (currentHour === blockHour) { // otherwise if the current time is same as block time
@@ -14,17 +14,25 @@ $(document).ready(function () { //Wrapped code to interact with the DOM
         } else if (currentHour < blockHour) { // otherwise if current time from the block time is less than current time
             $(this).addClass("future") // then its considered the future
         }
+        var blockHourId = $(this).attr("id") //this grabs the id for each time block from the html (9)
+        var reload = localStorage.getItem(blockHourId) // this shows the text content from the local storage
+        $(this).find(".description").text(reload) // this .text comand that puts the words on the page from ls
+        // console.log(reload)
+        // console.log(blockHourId)
     })
 
     $(".saveBtn").on("click", function () {  // adding a save to local is not working.
-        var textArea = $(this).prev().val(); 
-        var blockHour = ($(this).attr("id"));
-        console.log(blockHour);
-        // console.log("Hello");
-        console.log(textArea);
-        // localStorage.setItem("textarea",);
-        console.log("Saved Planner")
-    });
+        var textArea = $(this).prev().val();
+        var blockHourId = $(this).parent().attr("id");
+        // console.log(blockHour);
+        // // console.log("Hello");
+        // console.log(textArea);
+        localStorage.setItem(blockHourId, textArea);
+        // console.log("Saved Planner")
+    })
+
+
+    //var userStr = localStorage.getItem('user');
     // console.log("DOM",$(this)); - confirmed works
     // $(".saveBtn").on("click", function () { //added event listener to save user info to local.client side storage.
     //     // console.log("button",$(this)) - confirmed works
@@ -33,26 +41,9 @@ $(document).ready(function () { //Wrapped code to interact with the DOM
 
 
 
-// document.getElementById("reload").on("click", function () {
-//     document.getElementById("textarea").value = localStorage.getItem("textarea");
-//     alert("Reloaded Planner");
-//     console.log("Reloaded Planner")
-// }, false);
-
-//reload last session
-// document.getElementById("reload").addEventListener("click", function ()
-//     {
-//         document.getElementById("todo").value ='Your code';
-//         alert("Reloaded To-Do List") ;
-//         console.log("Reloaded To-Do List")
-//     } , false);
-
 // console.log("global",$(this)) - confirmed works
 //How can Day.js be used to get the current hour in 24-hour time?
 //var currentHour = dayjs().hour(); 
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
 
 
